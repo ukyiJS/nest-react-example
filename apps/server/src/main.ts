aa182007bcd7f2e3dd9362983b67c0a3a3d45961
ukyi <ukyi.js@gmail.com>
2024-11-06 23:18:49 +0900

    ➕ fastify 추가

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,11 +1,16 @@
 import { GLOBAL_PREFIX } from '@/shared/config/global-prefix.config.js';
-import { EnvService } from '@/shared/libs/env/env.service.js';
+import { EnvService } from '@/shared/lib/env/env.service.js';
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '@/app/app.module.js';
+import type { NestFastifyApplication } from '@nestjs/platform-fastify';
+import { FastifyAdapter } from '@nestjs/platform-fastify';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
   const env = app.get(EnvService);
   const port = env.get('PORT');
 
